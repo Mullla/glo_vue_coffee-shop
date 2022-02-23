e20bee3b4e966fc66b6e3e08413dbc981fe89a45
Mullla <diablo_13_06@mail.ru>
2022-02-23 12:05:27 +0300

    add: product card

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="best__item">
+    <img src="@/assets/img/coffee-2.jpg" alt="coffee" />
+    <div class="best__item-title">Presto Coffee Beans 1kg</div>
+    <div class="best__item-price">15.99$</div>
+  </div>
+</template>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -60,21 +60,9 @@
         <div class="row">
           <div class="col-lg-10 offset-lg-1">
             <div class="best__wrapper">
-              <div class="best__item">
-                <img src="@/assets/img/coffee-1.jpg" alt="coffee" />
-                <div class="best__item-title">Solimo Coffee Beans 2kg</div>
-                <div class="best__item-price">10.73$</div>
-              </div>
-              <div class="best__item">
-                <img src="@/assets/img/coffee-2.jpg" alt="coffee" />
-                <div class="best__item-title">Presto Coffee Beans 1kg</div>
-                <div class="best__item-price">15.99$</div>
-              </div>
-              <div class="best__item">
-                <img src="@/assets/img/coffee-3.jpg" alt="coffee" />
-                <div class="best__item-title">AROMISTICO Coffee 1kg</div>
-                <div class="best__item-price">6.99$</div>
-              </div>
+              <product-card />
+              <product-card />
+              <product-card />
             </div>
           </div>
         </div>
@@ -85,8 +73,12 @@
 
 <script>
 import AppNavbar from "@/components/AppNavbar";
+import ProductCard from "@/components/ProductCard";
 
 export default {
-  components: { AppNavbar },
+  components: {
+    AppNavbar,
+    ProductCard,
+  },
 };
 </script>
