ae43de2969319c56d56bc577ac819cf77be68946
Mullla <diablo_13_06@mail.ru>
2022-02-23 16:57:54 +0300

    fix: thank you title

--- src/views/ThankYouPage.vue
+++ src/views/ThankYouPage.vue
@@ -7,7 +7,7 @@
             <app-navbar />
           </div>
         </div>
-        <h1 class="title-big">Contact us</h1>
+        <h1 class="title-big">Thank you</h1>
       </div>
     </div>
     <section class="contacts">
