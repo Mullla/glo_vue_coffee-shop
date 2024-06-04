0750ec5e54e7dffc05b6fbab01b0d7ab994f712b
Mars <diablo_13_06@mail.ru>
2024-06-04 22:28:22 +0300

    update: AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <ul
+      :class="$style.nav"
+      class="
+        d-flex
+        justify-content-center
+        justify-content-md-start
+        flex-wrap
+      ">
+      <NavLink :to="navLinks.header.link" :class="$style.link">
+        <img :src="require(`@/assets/logo/${LINKS.header.icon}`)" alt="go to main" />
+      </NavLink>
+
+      <NavLink v-for="{ id, link, text } in LINKS.other" :key="id" :link="link" :text="text" :class="$style.link">
+        {{ text }}
+      </NavLink>
+    </ul>
+  </header>
+</template>
+
+<script setup>
+import NavLink from '@/components/NavLink.vue';
+import { LINKS } from '@/utils/constants';
+</script>
+
+<style module lang="scss">
+.nav {
+  display: flex;
+  align-items: flex-end;
+  margin-top: 30px;
+  list-style-type: none;
+}
+
+.link {
+    font-size: 12px;
+    padding: 10px 20px;
+    font-weight: normal;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    &:first-child {
+      transform: translateY(3px);
+    }
+
+    a {
+      color: #fff;
+
+      &:visited {
+          text-decoration: none;
+          color: #fff;
+      }
+
+      &:hover {
+          text-decoration: none;
+          color: #fff;
+      }
+    }
+  }
+</style>
